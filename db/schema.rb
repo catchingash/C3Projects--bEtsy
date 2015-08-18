52e248ce628f2b05c79c2111e1c6e4b27485b530
Loraine <loraine@lorainekv.com>
2015-08-18 15:22:27 -0700

    Updated the products table to include columns for package dimensions and weight.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818221621) do
+ActiveRecord::Schema.define(version: 20150818221911) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       null: false
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20150818221621) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.boolean  "retired",     default: false
+    t.string   "dimensions"
+    t.float    "weight"
   end
 
   create_table "products_categories", id: false, force: :cascade do |t|
