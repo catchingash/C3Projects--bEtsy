8c89f1bdc84cc22a4de89a7a21a9e921109c1f43
Ashley Watkins <AWatkins31@gmail.com>
2015-08-20 16:55:57 -0700

    Fixed tests that broke with added db columns.

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,7 +2,11 @@ FactoryGirl.define do
   factory :product do
     name "box"
     price 3
-    user_id 1
     stock 1
+    weight 1.500
+    length 10.000
+    width 5.500
+    height 15.500
+    user
   end
 end
