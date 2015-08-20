3dece3c1b24e7d2e6b75ad520eff5aa021bf2b31
Loraine <loraine@lorainekv.com>
2015-08-20 10:16:23 -0700

    Completely removed all shipping logic from orders controller.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,39 +1,13 @@
-require 'httparty'
 
 class OrdersController < ApplicationController
-  SHIPPING_URI = "http://localhost:3333/quotes"
-  ORIGIN_HASH = {origin_city: "Texarkana",
-                 origin_state: "TX",
-                 origin_zip: "75505",
-                 origin_country: "US"}
-  # Create an unless block to change this to rails.env during production
 
   before_action :set_order, only: [:cart, :checkout, :add_to_cart, :update, :receipt]
   before_action :set_seller_order, only: [:show]
   before_action :set_product, only: [:add_to_cart]
   before_action :set_seller, only: [:index, :show]
   before_action :require_seller_login, only: [:index, :show]
-  before_action :active_shipping_call, only: [:shipping]
 
   def cart; end
-
-  def active_shipping_call
-    get_destination
-    get_packages
-    query = { origin: ORIGIN_HASH, destination: @destination_hash, packages: @packages }
-    # hit the URI
-    # @response = HTTParty.get(SHIPPING_URI)
-    @response = HTTParty.get(SHIPPING_URI, query: query)
-  end
-
-  def shipping
-    @ups = @response["ups"]
-    @fedex = @response["fedex"]
-    # @ups = @response["ups"].rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price, rate.delivery_date]}
-    # @fedex = @response["fedex"].rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price, rate.delivery_date]}
-    # @ups_option = grab stuff from the hashy mash
-    # @fedex_option
-  end
 
   def checkout
     @order.prepare_checkout!
@@ -79,7 +53,6 @@ class OrdersController < ApplicationController
     @order_items = @order.order_items.select { |item| item.seller.id == @seller.id }
   end
 
-
   private
     def checkout_params
       params.require(:order).permit(:buyer_name, :buyer_email, :buyer_street, :buyer_city, :buyer_state, :buyer_zip, :buyer_country, :buyer_card_short, :buyer_card_expiration)
@@ -102,41 +75,4 @@ class OrdersController < ApplicationController
       @product = Product.find(params[:id])
     end
 
-    def get_destination
-      if session[:order_id].nil?
-        redirect_to cart_path
-      else
-        order = Order.find(session[:order_id])
-        @destination_hash = {
-          destination_city: order.buyer_city,
-          destination_state: order.buyer_state,
-          destination_zip: order.buyer_zip,
-          destination_country: order.buyer_country
-        }
-      end
-    end
-
-    def get_packages
-      if session[:order_id].nil?
-        redirect_to cart_path
-      else
-        order_items = OrderItem.where(order_id: session[:order_id])
-        items_hash = {}
-        counter = 1
-        order_items.each do |item|
-          product = Product.find(item.product_id)
-          each_item_hash= {
-            quantity: item.quantity_ordered,
-            weight: product.weight,
-            length: product.length,
-            width: product.width,
-            height: product.height
-          }
-          items_hash[counter] = each_item_hash
-          counter += 1
-        end
-        # items_string = items_hash.to_s.gsub(":", "").gsub("=>", ": ")
-        @packages = items_hash
-      end
-    end
 end
