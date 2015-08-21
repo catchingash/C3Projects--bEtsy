d168663362f659cb6e992e04743a628a2c2028a8
Loraine <loraine@lorainekv.com>
2015-08-21 13:15:55 -0700

    Added an orders factory.

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,15 @@ FactoryGirl.define do
     price 12345
     order_id 1
   end
+
+  factory :order do
+    buyer_name "Panda"
+    buyer_email "panda@panda.com"
+    buyer_card_short "3920"
+    buyer_street "123 Knockturn Alley"
+    buyer_city "Seattle"
+    buyer_state "WA"
+    buyer_zip "98115"
+  end
+
 end
