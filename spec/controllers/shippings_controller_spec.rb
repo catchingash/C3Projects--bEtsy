4c307d5deec4adbac63da151732d2adfb9f9c310
Loraine <loraine@lorainekv.com>
2015-08-21 14:00:00 -0700

    Failed attempt to write a get_packages spec when there is no order item session.

--- spec/controllers/shippings_controller_spec.rb
+++ spec/controllers/shippings_controller_spec.rb
@@ -16,12 +16,32 @@ RSpec.describe ShippingsController, type: :controller do
   end
 
   describe "get_packages" do
-    it "creates a hash containing packages for each order item" do
+
+    before(:each) do
       create :order
       create :product
+      create :product, name: "Another widget"
       create :order_item
-      session[:order_id] = 1
-      expect(controller.send(:get_packages)).to be_an_instance_of Hash
+      create :order_item, product_id: 2
     end
+
+    context "if there is an order session" do
+      it "creates a package hash with two order items" do
+        session[:order_id] = 1
+
+        expect(controller.send(:get_packages).count).to eq 2
+      end
+    end
+
+    # We don't think our redirect to root is working, it throws a
+    # Module::DelegationError:
+    # context "if there is no order in the session" do
+    #   it "redirects to the cart" do
+    #     session[:order_id] = nil
+    #     expect(controller.send(:get_packages)).to redirect_to(root_path)
+    #   end
+    # end
+
+
   end
 end
