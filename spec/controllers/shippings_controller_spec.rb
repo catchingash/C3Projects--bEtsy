37f189c237b00f808ce1757c96a877a3b9464653
Loraine <loraine@lorainekv.com>
2015-08-21 13:16:19 -0700

    WIP shipping controller spec.

--- spec/controllers/shippings_controller_spec.rb
+++ spec/controllers/shippings_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
+require "vcr_setup"
 
 RSpec.describe ShippingsController, type: :controller do
 
+  describe "api_shipping_call receives a response" do
+    #return to this later
+  end
+
+  describe "get_destination" do
+    create :order
+  end
 end
